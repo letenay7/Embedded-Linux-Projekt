2e38a6d8d22f0fd66ea63ef6eac9df8b9070fa2d
letenay7 <99814299+letenay7@users.noreply.github.com>
2023-02-09 18:43:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+
+
 git clone git://git.buildroot.net/buildroot
 
-#copy comfigs
-
-cp qemu_arm_mediaplayer_defconfig buildroot/configs/qemu_arm_mediaplayer_defconfig
+#copy config files
+cp qemu_arm_versatile_mediaplayer_defconfig buildroot/configs/qemu_arm_versatile_mediaplayer_defconfig
 cp mediaplayer.config buildroot/board/qemu/arm-versatile/mediaplayer.config
-
 
 cd buildroot
 make qemu_arm_versatile_mediaplayer_defconfig
 make
 
+cd ..
 cp S10music.sh buildroot/output/target/etc/init.d/S10music.sh
 chmod 775 buildroot/output/target/etc/init.d/S10music.sh
-cd .. 
 
 #handle music
 
